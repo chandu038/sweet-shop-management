090905ac85cd00e8321a104ec66b2ada80e73e90
chandu038 <chandudarapaneni093@gmail.com>
2025-12-14 14:58:56 +0530

    fix: Add unique constraint to username and reset database

--- backend/src/main/java/com/Incubyte/Sweet_Shop_Backend/Model/User.java
+++ backend/src/main/java/com/Incubyte/Sweet_Shop_Backend/Model/User.java
@@ -13,8 +13,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true, nullable = false) 
     private String username;
+    @Column(nullable = false)
     private String password;
     private String role; // "USER" or "ADMIN"
 }
